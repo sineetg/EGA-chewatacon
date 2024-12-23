cf4bdce4cd14e5413064b317370478e1d63ff042
sineetg <wrsineety@gmail.com>
2024-12-23 13:30:26 +0300

    All functions working

--- CHEWATACON/wsgi.py
+++ CHEWATACON/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CHEWATACON.settings')
 
 application = get_wsgi_application()
+
+# To connect to the django app on vercel
+app = application
